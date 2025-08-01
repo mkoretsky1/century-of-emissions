de84cb9a9a060b76927a120ff2c62593fbf7c23c
Mathew Koretsky <mathew.koretsky1@gmail.com>
2025-08-01 19:03:16 -0400

    editing new formed annotations

--- js/scene1.js
+++ js/scene1.js
@@ -6,23 +6,23 @@ function scene1() {
     annotations: [
       {
         type: "label",
-        year: 1945,
-        country: "Ukraine",
-        dx: 50,
-        dy: -200,
-        wrap: 250,
-        title: "World War II Ends (September 1945)",
-        text:  "The end of history's deadliest war starts an era of unprecedented economic growth. In turn, CO₂ emissions begin to increase rapidly."
-      },
-      {
-        type: "label",
         year: 1990,
         country: "Ukraine",
-        dx: -200,
+        dx: -100,
         dy: -10,
         wrap: 250,
         title: "Emissions Explode",
         text: "The total CO₂ output from the top 10 emitting countries increases around 3.5x in just over 50 years."
+      },
+      {
+        type: "label",
+        year: 1945,
+        country: "Ukraine",
+        dx: 50,
+        dy: -150,
+        wrap: 250,
+        title: "World War II Ends (September 1945)",
+        text:  "The end of history's deadliest war starts an era of unprecedented economic growth. In turn, CO₂ emissions begin to increase rapidly."
       }
     ]
   });
